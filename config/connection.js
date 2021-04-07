3fe6d5e960e81b541782ee0c2873a54aa60b5a53
Muhammed Rahif <rahifpalliyalil@gmail.com>
2021-04-07 08:54:52 +0530

    Added some configurations

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,21 @@
-const mongoClient = require('mongodb').MongoClient;
-const state={
-    db:null
-}
+const mongoClient = require("mongodb").MongoClient;
+const state = {
+  db: null,
+};
 
-module.exports.connect=(done)=>{
-    const url='mongodb://localhost:27017'
-    const dbname='AriyippukalOnline'
+module.exports.connect = (done) => {
+  const url = process.env.MONGODB_URI || "mongodb://localhost:27017";
+  const dbname = "AriyippukalOnline";
 
-    mongoClient.connect(url,(err,data)=>{
-        if (err) {
-            return done(err)
-        }else{
-            state.db=data.db(dbname)
-            done()
-        }
-    })
-}
-module.exports.get=()=>{
-    return state.db
-}
+  mongoClient.connect(url, (err, data) => {
+    if (err) {
+      return done(err);
+    } else {
+      state.db = data.db(dbname);
+      done();
+    }
+  });
+};
+module.exports.get = () => {
+  return state.db;
+};
